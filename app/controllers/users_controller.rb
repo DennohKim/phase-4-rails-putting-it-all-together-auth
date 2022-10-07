0d456bdf3a9cbb1727d3d2b82b7343315c136604
Dennis Kimathi <kimathidennis38@gmail.com>
2022-10-07 11:45:22 +0300

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,40 +14,9 @@ class UsersController < ApplicationController
 
   private
 
+
   def user_params
     params.permit(:username, :password, :password_confirmation, :image_url, :bio)
   end
-
-  # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-  # before_action :authorize, only: [:show]
-
-  # def create
-  #   user = User.create(user_params)
-  #   if user.valid?
-  #     session[:user_id] = user.id
-  #     render json: user, status: :created
-  #   else
-  #     render json: { error: user.errors.full_messages }, status: :unprocessable_entity
-  #   end
-  # end
-
-  # def show
-  #   user = User.find_by(id: session[:user_id])
-  #   render json: user
-  # end
-
-  # private
-
-  # def authorize
-  #   return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
-  # end
-
-  # def user_params
-  #   params.permit(:username, :password, :password_confirmation, :image_url, :bio)
-  # end
-
-  # def render_unprocessable_entity_response(invalid)
-  #     render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
-  # end
 
 end
